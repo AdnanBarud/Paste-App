3c73f5b7fc4bbe5b11dd3614b0fe207ac48648fe
AdnanBarud <barudyt@gmail.com>
2025-12-06 12:56:47 +0530

    creating pastes page

--- src/components/Pastes.jsx
+++ src/components/Pastes.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import toLowerCase from 'react'
 
 const pastes = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const pastes = useSelector( (state) => state.paste.pastes );
+
+  const filterPastes =  pastes.filter(
+    (paste) => paste.title.toLowerCase().includes().searchTerm.toLowerCase()
+  )
+ 
   return (
-    <div>pastes</div>
+    <div>
+      <input 
+      className='rounded-2xl border-gray-300 bg-black mt-4 p-2 pl-4 min-w-[700px]'
+      type="search"
+      placeholder='search here'
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(searchTerm)}
+      />
+    </div>
   )
 }
 
