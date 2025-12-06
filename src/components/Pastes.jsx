87b7a73345a1d09b9041d094e5ead73eaa77670d
AdnanBarud <barudyt@gmail.com>
2025-12-06 14:44:11 +0530

    logic for buttons

--- src/components/Pastes.jsx
+++ src/components/Pastes.jsx
@@ -1,16 +1,43 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { removeFromPastes } from '../redux/pasteSlice';
+
 
 
 const pastes = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+
+  const dispatch = useDispatch();
+
   const pastes = useSelector( (state) => state.paste.pastes );
 
   const filterPastes =  pastes.filter(
     (paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase())
   )
  
+  // handlers for edit, copy, view, delete, share
+  function handleEdit() {
+    // implement edit functionality
+     
+
+  }
+  function handleCopy(paste) {
+    // implement copy functionality
+    navigator.clipboard.writeText(paste?.content);
+    toast.success('Copied to clipboard!');
+  }
+  function handleView() {
+    // implement view functionality
+  } 
+  function handleDelete(pasteId) {
+    // implement delete functionality
+    dispatch(removeFromPastes(pasteId));
+  } 
+  function handleShare() {
+    // implement share functionality
+  }
   return (
     <div>
       <input 
@@ -20,6 +47,47 @@ const pastes = () => {
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)}
       />
+      
+      <div>
+
+        {
+        filterPastes.length > 0 
+        && filterPastes.map( 
+          (paste) => {
+            return (
+              <div className='border' key={paste._id}>
+                <div>
+                  {paste.title}
+                </div>
+                <div>
+                  {paste.content}
+                </div>
+                <div>
+                  <button onClick={() => handleEdit(paste?._id)}>
+                    Edit
+                  </button>
+                  <button onClick={() => handleCopy(paste)}>
+                    Copy
+                  </button>
+                  <button onClick={handleView}>
+                    View
+                  </button>
+                  <button onClick={handleDelete}>
+                    Delete
+                  </button>
+                  <button onClick={handleShare}>
+                    Share
+                  </button>
+                </div>
+
+              </div>
+              
+            )
+          }
+        )
+        }
+      
+      </div>
     </div>
   )
 }
